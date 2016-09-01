b958eaadedb06d16330eb7d05046ca53ad6c98e9
dkmehrmann <dkmehrmann@gmail.com>
2016-08-31 19:51:22 -0500

    testing

--- Week3/assignments/scripts/w3p1.sh
+++ Week3/assignments/scripts/w3p1.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-wget http://vbcity.com/cfs-filesystemfile.ashx/__key/CommunityServer.Components.PostAttachments/00.00.61.18.99/Country-List.txt
+cd ~/w3p1_testdir/subdir
+
+wget https://raw.githubusercontent.com/cervoise/pentest-scripts/master/password-cracking/wordlists/pokemon-list-en.txt
 
 while read p; do
   mkdir $p
-done <Country-List.txt
+done <pokemon-list-en.txt
